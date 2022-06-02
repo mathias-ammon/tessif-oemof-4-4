eb82f7ee52a429be1adec70f87739febe19890ae
tZE <WriteMe@NoEmail.net>
2022-06-02 11:01:02 +0200

    Try ommiting pre-cmmot virtualenv hook

--- noxfile.py
+++ noxfile.py
@@ -5,10 +5,12 @@
 import shutil
 import tempfile
 from pathlib import Path
-from textwrap import dedent
 
 # third pary packages
 import nox
+
+# from textwrap import dedent
+
 
 # local packages
 
@@ -175,56 +177,56 @@ def codecov(session):
     session.run("codecov", *session.posargs)
 
 
-def activate_virtualenv_in_precommit_hooks(session):
-    """Activate virtualenv in hooks installed by pre-commit.
+# def activate_virtualenv_in_precommit_hooks(session):
+#     """Activate virtualenv in hooks installed by pre-commit.
 
-    This function patches git hooks installed by pre-commit to activate the
-    session's virtual environment. This allows pre-commit to locate hooks in
-    that environment when invoked from git.
+#     This function patches git hooks installed by pre-commit to activate the
+#     session's virtual environment. This allows pre-commit to locate hooks in
+#     that environment when invoked from git.
 
-    Parameters
-    ----------
-    session
-        The Session object.
-    """
-    assert session.bin is not None  # noqa: S101
+#     Parameters
+#     ----------
+#     session
+#         The Session object.
+#     """
+#     assert session.bin is not None  # noqa: S101
 
-    virtualenv = session.env.get("VIRTUAL_ENV")
-    if virtualenv is None:
-        return
+#     virtualenv = session.env.get("VIRTUAL_ENV")
+#     if virtualenv is None:
+#         return
 
-    hookdir = Path(".git") / "hooks"
-    if not hookdir.is_dir():
-        return
+#     hookdir = Path(".git") / "hooks"
+#     if not hookdir.is_dir():
+#         return
 
-    for hook in hookdir.iterdir():
-        if hook.name.endswith(".sample") or not hook.is_file():
-            continue
+#     for hook in hookdir.iterdir():
+#         if hook.name.endswith(".sample") or not hook.is_file():
+#             continue
 
-        text = hook.read_text()
-        bindir = repr(session.bin)[1:-1]  # strip quotes
-        if not (
-            Path("A") == Path("a") and bindir.lower() in text.lower() or bindir in text
-        ):
-            continue
+#         text = hook.read_text()
+#         bindir = repr(session.bin)[1:-1]  # strip quotes
+#         if not (
+#             Path("A") == Path("a") and bindir.lower() in text.lower() or bindir in text
+#         ):
+#             continue
 
-        lines = text.splitlines()
-        if not (lines[0].startswith("#!") and "python" in lines[0].lower()):
-            continue
+#         lines = text.splitlines()
+#         if not (lines[0].startswith("#!") and "python" in lines[0].lower()):
+#             continue
 
-        header = dedent(
-            f"""\
-            import os
-            os.environ["VIRTUAL_ENV"] = {virtualenv!r}
-            os.environ["PATH"] = os.pathsep.join((
-                {session.bin!r},
-                os.environ.get("PATH", ""),
-            ))
-            """
-        )
+#         header = dedent(
+#             f"""\
+#             import os
+#             os.environ["VIRTUAL_ENV"] = {virtualenv!r}
+#             os.environ["PATH"] = os.pathsep.join((
+#                 {session.bin!r},
+#                 os.environ.get("PATH", ""),
+#             ))
+#             """
+#         )
 
-        lines.insert(1, header)
-        hook.write_text("\n".join(lines))
+#         lines.insert(1, header)
+#         hook.write_text("\n".join(lines))
 
 
 @nox.session(name="pre-commit", python="3.10")
@@ -248,8 +250,6 @@ def precommit(session):
         "pyupgrade",
     )
     session.run("pre-commit", *args)
-    if args and args[0] == "install":
-        activate_virtualenv_in_precommit_hooks(session)
 
 
 @nox.session(python="3.10")
